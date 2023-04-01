9afae98ab360361702cfeb3fdfafed5a3edfc102
manassingh19 <129588675+manassingh19@users.noreply.github.com>
2023-04-01 15:54:13 +0530

    Update answer3.sh

--- answer3.sh
+++ answer3.sh
@@ -1,12 +1,26 @@
-object='{"a":{"b":{"c":"d"}}}'
-key='a/b/c'
-value=$(get_value "$object" "$key")
-echo "$value" # Output: d
+#!/bin/bash
 
-object='{"x":{"y":{"z":"a"}}}'
-key='x/y/z'
-value=$(get_value "$object" "$key")
-echo "$value" # Output: a
+function get_value {
+  local object=$1
+  local key=$2
+
+  # Convert key string into array
+  IFS="/" read -ra key_arr <<< "$key"
+
+  # Traverse the object using the keys
+  local current="$object"
+  for k in "${key_arr[@]}"; do
+    if [[ -v "$current[$k]" ]]; then
+      current="${current[$k]}"
+    else
+      echo "Key not found: $key" >&2
+      return 1
+    fi
+  done
+
+  echo "$current"
+}
+
 
 
 object='{"a":{"b":{"c":"d"}}}'
